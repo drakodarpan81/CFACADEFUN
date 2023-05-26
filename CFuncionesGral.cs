cd7d72e6cf23db9618e2c19e4e745eb40c22cc07
drakodarpan81 <drakodarpan@gmail.com>
2023-05-26 10:04:46 -0700

    first commit

---
+++ CFuncionesGral.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFACADEFUN
+{
+    public class CFuncionesGral
+    {
+        public static string GeneraPassWord(string sUsuario, string sBaseDeDatos)
+        {
+            string sCadena = "";
+            sCadena = sUsuario + "-" + sBaseDeDatos;
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(sCadena));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public static string consultarsIp()
+        {
+            IPHostEntry host;
+            string sIpLocal = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    sIpLocal = ip.ToString().Replace(".", "").Trim();
+                }
+            }
+
+            return sIpLocal;
+        }
+    }
+}
